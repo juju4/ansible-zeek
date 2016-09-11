b39181f5d519a59ea6e6cf30e73e522510a7a206
juju4 <juju4@users.noreply.github.com>
2016-09-11 13:50:48 -0400

    sync repositories + travis test: docker

---
+++ get-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+rolesdir=$(dirname $0)/..
+
+[ ! -d $rolesdir/maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/maxmind
+[ ! -d $rolesdir/redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/redhat-epel
+
