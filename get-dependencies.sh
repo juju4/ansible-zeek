f80301d5f34fd3131986bb54b2a9f80c1e56c1be
juju4 <juju4@users.noreply.github.com>
2017-01-03 09:07:00 -0500

    add simple Jenkinsfile

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,4 +8,6 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.ipsumdump ] && git clone https://github.com/juju4/ansible-ipsumdump $rolesdir/juju4.ipsumdump
+## galaxy naming
+[ ! -e $rolesdir/juju4.bro-ids ] && ln -s ansible-bro-ids $rolesdir/juju4.bro-ids
 
