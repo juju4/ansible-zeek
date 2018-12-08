8c373c0e477ea2a1d43c05978ea853149a439f31
juju4 <juju4@users.noreply.github.com>
2018-12-08 17:10:39 -0500

    update galaxy naming + lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,12 +6,11 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.ipsumdump ] && git clone https://github.com/juju4/ansible-ipsumdump $rolesdir/juju4.ipsumdump
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.bro-ids ] && ln -s ansible-bro-ids $rolesdir/juju4.bro-ids
-[ ! -e $rolesdir/juju4.bro-ids ] && cp -R $rolesdir/ansible-bro-ids $rolesdir/juju4.bro-ids
+[ ! -e $rolesdir/juju4.broids ] && cp -R $rolesdir/ansible-bro-ids $rolesdir/juju4.broids
 
 ## don't stop build on this script return code
 true
-
