75b8f0ec2a1f3c6468153180c70eabc4ecff642a
juju4 <juju4@users.noreply.github.com>
2021-01-17 14:22:11 -0500

    fix flake8

--- files/api.py
+++ files/api.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
-## from http://nullsecure.org/building-your-own-passivedns-feed/
+# from http://nullsecure.org/building-your-own-passivedns-feed/
 
-import requests  
-import json  
+import requests
+import json
 import sys
 
 indicator = sys.argv[1]
 
 url = "http://localhost:8081/dns/"
 
-r = requests.get(url+indicator)  
+r = requests.get(url+indicator)
 j = json.loads(r.text)
 
-print "+------------------------+------------------------+--------+-------+-----------------------------+"  
-print "|       First Seen       |        Last Seen       |  Type  |  TTL  |           Answer            |"  
-print "+------------------------+------------------------+--------+-------+-----------------------------+"
+print("+------------------------+------------------------+--------+-------+-----------------------------+")
+print("|       First Seen       |        Last Seen       |  Type  |  TTL  |           Answer            |")
+print("+------------------------+------------------------+--------+-------+-----------------------------+")
 
-for record in j['records']:  
-    print "  ",record['first'],"\t   ",record['last'],"     ",record['type'],"      ",record['ttl'],"   ",record['answer']
-
+for record in j['records']:
+    print("  ", record['first'], "\t   ", record['last'], "     ",
+          record['type'], "      ", record['ttl'], "   ", record['answer'])
